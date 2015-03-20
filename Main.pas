5dabeb24eb48b9a25369ed42b8c196b661a0f737
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2015-03-20 15:04:50 +0100

    Add players and pads

--- Main.pas
+++ Main.pas
@@ -7,28 +7,91 @@ procedure Run;
 implementation
 
 uses
-  Allegro5;
+  Allegro5,
+  al5primitives;
+
+type
+  TPlayer = record
+    x, y: Integer;
+    vx, vy: Integer;
+  end;
+
+  TPad= record
+    x, y: Integer;
+    vx, vy: Integer;
+  end;
 
 var
   Display: ALLEGRO_DISPLAYptr;
   Queue: ALLEGRO_EVENT_QUEUEptr;
   Timer: ALLEGRO_TIMERptr;
+  Player: TPlayer;
+  Pad1, Pad2: TPad;
 
 procedure Init;
 begin
   al_init;
   Display := al_create_display(1920, 1080);
+
+  al_install_keyboard;
+
   Timer := al_create_timer(1.0 / 60.0);
   Queue := al_create_event_queue;
+
+  al_init_primitives_addon;
+
   al_register_event_source(Queue, al_get_display_event_source(display));
   al_register_event_source(Queue, al_get_timer_event_source(Timer));
-  al_start_timer(timer);
+  al_register_event_source(Queue, al_get_keyboard_event_source);
+  al_start_timer(Timer);
+
+  Player.x := 100;
+  Player.y := 100;
+  Player.vx := 0;
+  Player.vy := 0;
+
+  Pad1.x := 0;
+  Pad1.y := 0;
+  Pad1.vx := 0;
+  Pad1.vy := 0;
+
+  Pad2.x := 0;
+  Pad2.y := 0;
+  Pad2.vx := 0;
+  Pad2.vy := 0;
+end;
+
+procedure DrawPlayer(var Player: TPlayer);
+begin
+  al_draw_filled_rectangle(Player.x, Player.y, Player.x+10, Player.y+10, al_map_rgb(255, 255, 255));
+end;
+
+procedure DrawPad(var Pad: TPad);
+begin
+  al_draw_filled_rectangle(Pad.x, Pad.y, Pad.x+5, Pad.y+20, al_map_rgb(255,0,0));
 end;
 
 procedure Draw;
 begin
   al_clear_to_color(al_map_rgb(0, 0, 0));
+
+  DrawPlayer(Player);
+  DrawPad(Pad1);
+  DrawPad(Pad2);
+
   al_flip_display;
+end;
+
+procedure Update;
+begin
+  Player.x := Player.x + Player.vx;
+  Player.y := Player.y + Player.vy;
+
+  Pad1.x := Pad1.x + Pad1.vx;
+  Pad1.y := Pad1.y + Pad1.vy;
+
+  Pad2.x := Pad2.x + Pad2.vx;
+  Pad2.y := Pad2.y + Pad2.vy;
 end;
 
 procedure Loop;
@@ -42,7 +105,11 @@ begin
   while IsRunning do
   begin
     if (ShouldDraw) and (al_is_event_queue_empty(Queue)) then
+    begin
+      Update;
       Draw;
+      ShouldDraw := False;
+    end;
 
     al_wait_for_event(Queue, Event);
 
@@ -51,6 +118,24 @@ begin
         IsRunning := False;
       ALLEGRO_EVENT_TIMER:
         ShouldDraw := True;
+      ALLEGRO_EVENT_KEY_DOWN:
+        begin
+          case Event.Keyboard.KeyCode of
+            ALLEGRO_KEY_S:
+              Inc(Player.vy);
+            ALLEGRO_KEY_W:
+              Dec(Player.vy);
+            ALLEGRO_KEY_D:
+              Inc(Player.vx);
+            ALLEGRO_KEY_A:
+              Dec(Player.vx);
+            ALLEGRO_KEY_ESCAPE:
+              IsRunning := False;
+          end;
+        end;
+      ALLEGRO_EVENT_KEY_UP:
+        begin
+        end;
     end;
   end;
 end;
--- Makefile
+++ Makefile
@@ -1,19 +1,20 @@
 all:
-	@fpc MiniLD58.dpr -l- -FUobj -FEbin -Fulib/allegro-pas5/lib
+	@fpc MiniLD58.dpr -B -l- -FUobj -FEbin -Fulib/allegro-pas5/lib -dMONOLITH
 
 delphi:
-	@DCC32 MiniLD58.dpr -l- -Nobj -Ebin -CC
+	@DCC32 MiniLD58.dpr -l- -Nobj -Ebin -CC -Ulib/allegro-pas5/lib -dMONOLITH
 
 clean:
-	@rm -f *.o
-	@rm -f *.~*
-	@rm -f *.ppu
-	@rm -f *.dcu
-	@rm -f *.exe
-	@rm -f bin/*.*
-	@rm -f obj/*.*
-	@touch bin/.gitkeep
-	@touch obj/.gitkeep
+	@rm -f *.o \
+	       *.~* \
+	       *.ppu \
+	       *.dcu \
+	       *.exe \
+	       bin/*.exe \
+	       obj/*.o \
+	       obj/*.a \
+	       obj/*.dcu \
+	       obj/*.ppu
 
 try: all run
 
