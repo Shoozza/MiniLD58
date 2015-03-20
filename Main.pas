1b99330b6b173d6a6f44e8031b7c383505763cce
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2015-03-20 12:31:14 +0100

    Add init, loop and clean procs

--- Main.pas
+++ Main.pas
@@ -6,9 +6,34 @@ procedure Run;
 
 implementation
 
+procedure Init;
+begin
+end;
+
+procedure Loop;
+var
+  IsRunning: boolean;
+begin
+  IsRunning := True;
+
+  While IsRunning do
+  begin
+    IsRunning := False;
+  end;
+end;
+
+procedure Clean;
+begin
+end;
+
 procedure Run;
 begin
   Writeln('Start');
+
+  Init;
+  Loop;
+  Clean;
+
   Writeln('End');
 end;
 
