b03cbd07ad0b99fdabdb52ceb39fec12cd54e7a3
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2015-03-21 12:02:28 +0100

    Add player shade to coins

--- Main.pas
+++ Main.pas
@@ -67,6 +67,7 @@ var
   Score: Integer;
   BestScore: Integer;
   Font: ALLEGRO_FONTptr;
+  Pause: Boolean;
 
 procedure Init;
 var
@@ -155,6 +156,7 @@ begin
 
   KeyW := False;
   KeyS := False;
+  Pause := False;
 
   Shake := 0;
   ShakeX := 0;
@@ -304,15 +306,119 @@ begin
 end;
 
 procedure DrawCoin(var Coin: TCoin; var PadColor: ALLEGRO_COLOR; var PadShadeColor: ALLEGRO_COLOR);
+var
+  x1, y1: Integer;
+  x2, y2: Integer;
+  x3, y3: Integer;
+const
+  LEN = 10;
 begin
+  x1 := 0;
+  x2 := 0;
+  x3 := 0;
+
+  y1 := 0;
+  y2 := 0;
+  y3 := 0;
+
+  if Coin.vx = 0 then
+  begin
+    if Coin.vy > 0 then
+    begin
+      x1 := Coin.x;
+      x2 := Coin.x + Coin.w;
+      x3 := Coin.x + Coin.w div 2;
+
+      y1 := Coin.y;
+      y2 := Coin.y;
+      y3 := Coin.y - Coin.vy*LEN;
+    end
+    else if Coin.vy < 0 then
+    begin
+      x1 := Coin.x;
+      x2 := Coin.x + Coin.w;
+      x3 := Coin.x + Coin.w div 2;
+
+      y1 := Coin.y + Coin.h;
+      y2 := Coin.y + Coin.h;
+      y3 := Coin.y + Coin.h - Coin.vy*LEN;
+    end
+  end
+  else if Coin.vy = 0 then
+  begin
+    if Coin.vx > 0 then
+    begin
+      x1 := Coin.x;
+      x2 := Coin.x;
+      x3 := Coin.x - Coin.vx*LEN;
+
+      y1 := Coin.y;
+      y2 := Coin.y + Coin.h;
+      y3 := Coin.y + Coin.h div 2;
+    end
+    else if Coin.vx < 0 then
+    begin
+      x1 := Coin.x + Coin.w;
+      x2 := Coin.x + Coin.w;
+      x3 := Coin.x + Coin.w - Coin.vx*LEN;
+
+      y1 := Coin.y;
+      y2 := Coin.y + Coin.h;
+      y3 := Coin.y + Coin.h div 2;
+    end
+  end
+  else if Coin.vy > 0 then
+  begin
+    if Coin.vx > 0 then
+    begin
+      x1 := Coin.x;
+      x2 := Coin.x + Coin.w;
+      x3 := Coin.x - Coin.vx*LEN;
+
+      y1 := Coin.y + Coin.h;
+      y2 := Coin.y;
+      y3 := Coin.y - Coin.vy*LEN;
+    end
+    else if Coin.vx < 0 then
+    begin
+      x1 := Coin.x;
+      x2 := Coin.x + Coin.w;
+      x3 := Coin.x + Coin.w - Coin.vx*LEN;
+
+      y1 := Coin.y;
+      y2 := Coin.y + Coin.h;
+      y3 := Coin.y - Coin.vy*LEN;
+    end;
+  end
+  else if Coin.vy < 0 then
+  begin
+    if Coin.vx > 0 then
+    begin
+      x1 := Coin.x;
+      x2 := Coin.x + Coin.w;
+      x3 := Coin.x - Coin.vx*LEN;
+
+      y1 := Coin.y;
+      y2 := Coin.y + Coin.h;
+      y3 := Coin.y + Coin.h - Coin.vy*LEN;
+    end
+    else if Coin.vx < 0 then
+    begin
+      x1 := Coin.x;
+      x2 := Coin.x + Coin.w;
+      x3 := Coin.x + Coin.w - Coin.vx*LEN;
+
+      y1 := Coin.y + Coin.h;
+      y2 := Coin.y;
+      y3 := Coin.y + Coin.h - Coin.vy*LEN;
+    end;
+  end;
+
+  al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, CoinShadeColor);
   if Coin.Active > 30 then
     al_draw_filled_rectangle(Coin.x, Coin.y, Coin.x+Coin.w, Coin.y+Coin.h, CoinColor)
   else
-    al_draw_filled_rectangle(Coin.x, Coin.y, Coin.x+Coin.w, Coin.y+Coin.h, CoinShadeColor);
-  if Coin.vy > 0 then
-    al_draw_filled_rectangle(Coin.x, Coin.y-Coin.vy*10, Coin.x+Coin.w, Coin.y, CoinShadeColor)
-  else if Coin.vy < 0 then
-    al_draw_filled_rectangle(Coin.x, Coin.y+Coin.h, Coin.x+Coin.w, Coin.y+Coin.h-Coin.vy*10, CoinShadeColor);
+    al_draw_filled_rectangle(Coin.x, Coin.y, Coin.x+Coin.w, Coin.y+Coin.h, CoinColor);
 end;
 
 procedure DrawPad(var Pad: TPad; var PadColor: ALLEGRO_COLOR; var PadShadeColor: ALLEGRO_COLOR);
@@ -359,6 +465,16 @@ begin
   end;
   DrawPlayer(Player);
 
+  if Pause then
+  begin
+    al_draw_filled_rectangle(0, 0,
+      Settings.Width, Settings.Height,
+      al_map_rgba(0, 0, 0, 40));
+    al_draw_filled_rectangle(Settings.Width / 20, Settings.Height / 20,
+      Settings.Width - Settings.Width / 20, Settings.Height - Settings.Height / 20,
+      al_map_rgba(0, 0, 0, 40));
+  end;
+
   al_flip_display;
 end;
 
@@ -367,14 +483,17 @@ var
   I: Integer;
   N: Integer;
 begin
+  if Pause then
+    Exit;
+
   N := random(MAX_COINS) + 1;
   if Coins[N].Active = 0 then
   begin
     Coins[N].Active := random(40) + 300;
     Coins[N].x := random(Settings.Width div 2) + Settings.Width div 4;
     Coins[N].y := random(Settings.Height div 2) + Settings.Height div 4;
-    Coins[N].vx := 0;
-    Coins[N].vy := 0;
+    Coins[N].vx := random(3) - 2;
+    Coins[N].vy := random(9) - 5;
   end;
 
   // move
@@ -504,6 +623,9 @@ end;
 
 procedure HandleInput;
 begin
+  if Pause then
+    Exit;
+
   if KeyW then
   begin
     if Player.vy > 0 then
@@ -559,17 +681,20 @@ begin
             ALLEGRO_KEY_W:
               KeyW := True;
             ALLEGRO_KEY_D:
-              if Player.vx = 0 then
+              if (not Pause) and (Player.vx = 0) then
               begin
                 Player.vx := 10;
                 al_play_sample(SpawnSound, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, nil)
               end;
             ALLEGRO_KEY_A:
-              if Player.vx = 0 then
+              if (not Pause) and (Player.vx = 0) then
               begin
                 Player.vx := -10;
                 al_play_sample(SpawnSound, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, nil)
               end;
+            ALLEGRO_KEY_P:
+              Pause := not Pause;
+
             ALLEGRO_KEY_ESCAPE:
               IsRunning := False;
           end;
