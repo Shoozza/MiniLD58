f41ecdc895251493f2a5ea0a42465a30f4397a66
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2015-03-21 11:13:13 +0100

    Add scores

--- Main.pas
+++ Main.pas
@@ -12,7 +12,10 @@ uses
   Allegro5,
   al5primitives,
   al5audio,
-  al5acodec;
+  al5acodec,
+  al5font,
+  al5ttf,
+  al5Image;
 
 type
   TPlayer = record
@@ -27,9 +30,19 @@ type
     vx, vy: Integer;
   end;
 
+  TCoin = record
+    x, y: Integer;
+    w, h: Integer;
+    vx, vy: Integer;
+    Active: Integer;
+  end;
+
   TSettings = record
     Width, Height: Integer;
   end;
+
+const
+  MAX_COINS = 2;
 
 var
   Display: ALLEGRO_DISPLAYptr;
@@ -37,10 +50,12 @@ var
   Timer: ALLEGRO_TIMERptr;
   Player: TPlayer;
   Pad1, Pad2: TPad;
+  Coins: Array [1..MAX_COINS] of TCoin;
   Settings: TSettings;
   Ini: TMemIniFile;
   BackgroundColor, BackgroundShadeColor,
     PlayerColor, PlayerShadeColor,
+    CoinColor, CoinShadeColor,
     LeftPadColor, LeftPadShadeColor,
     RightPadColor, RightPadShadeColor: ALLEGRO_COLOR;
   WallSound, LeftPadSound, RightPadSound,
@@ -49,8 +64,13 @@ var
   Shake: Integer;
   ShakeX, ShakeY: Single;
   KeyS, KeyW: Boolean;
+  Score: Integer;
+  BestScore: Integer;
+  Font: ALLEGRO_FONTptr;
 
 procedure Init;
+var
+  I: Integer;
 begin
   SetCurrentDir(ExtractFilePath(ParamStr(0)));
   Ini := TMemIniFile.Create('config.ini');
@@ -62,17 +82,22 @@ begin
   Display := al_create_display(Settings.Width, Settings.Height);
 
   Randomize;
-  al_install_keyboard;
+
+  al_init_image_addon;
+  al_init_font_addon;
+  al_init_ttf_addon;
+  al_init_primitives_addon;
+
   al_init_acodec_addon;
   if not al_install_audio then
     Writeln('error: al_install_audio');
   if not al_reserve_samples(16) then
     Writeln('error: al_reserve_samples');
 
+  al_install_keyboard;
+
   Timer := al_create_timer(1.0 / 60.0);
   Queue := al_create_event_queue;
-
-  al_init_primitives_addon;
 
   al_register_event_source(Queue, al_get_display_event_source(display));
   al_register_event_source(Queue, al_get_timer_event_source(Timer));
@@ -103,6 +128,10 @@ begin
   if PointSound = nil then
     Writeln('Error: loading ' + GetCurrentDir + '\point.wav');
 
+  Font := al_load_font(GetCurrentDir + '\font.ttf', 7*72, 0);
+  if Font = nil then
+    WriteLn('Error: loading ttf font');
+
   BackgroundColor := al_map_rgb(255, 255, 255);
   BackgroundShadeColor := al_map_rgb(235, 235, 235);
   PlayerColor := al_map_rgb(248, 180, 93);
@@ -111,6 +140,18 @@ begin
   LeftPadShadeColor := al_map_rgb(199, 219, 246);
   RightPadColor := al_map_rgb(230,  98, 98);
   RightPadShadeColor := al_map_rgb(251, 185, 185);
+  CoinColor := al_map_rgb(175, 223, 142);
+  CoinShadeColor := al_map_rgb(213, 228, 202);
+
+  for I := 1 to MAX_COINS do
+  begin
+    Coins[I].Active := 0;
+    Coins[I].w := 80;
+    Coins[I].h := 80;
+  end;
+
+  BestScore := 0;
+  Score := 0;
 
   KeyW := False;
   KeyS := False;
@@ -139,6 +180,14 @@ begin
   Pad2.h := 180;
   Pad2.vx := 0;
   Pad2.vy := -4;
+end;
+
+procedure DrawScore;
+begin
+  al_draw_text(Font, al_map_rgb(230, 230, 230), Settings.Width div 4, Settings.Height div 4,
+    ALLEGRO_ALIGN_CENTRE, IntToStr(Score));
+  al_draw_text(Font, al_map_rgb(230, 230, 230), Settings.Width div 4 * 3, Settings.Height div 4,
+    ALLEGRO_ALIGN_CENTRE, IntToStr(BestScore));
 end;
 
 procedure DrawPlayer(var Player: TPlayer);
@@ -254,6 +303,18 @@ begin
   al_draw_filled_rectangle(Player.x+ShakeX, Player.y+ShakeY, Player.x+Player.w+ShakeX, Player.y+Player.h+ShakeY, PlayerColor);
 end;
 
+procedure DrawCoin(var Coin: TCoin; var PadColor: ALLEGRO_COLOR; var PadShadeColor: ALLEGRO_COLOR);
+begin
+  if Coin.Active > 30 then
+    al_draw_filled_rectangle(Coin.x, Coin.y, Coin.x+Coin.w, Coin.y+Coin.h, CoinColor)
+  else
+    al_draw_filled_rectangle(Coin.x, Coin.y, Coin.x+Coin.w, Coin.y+Coin.h, CoinShadeColor);
+  if Coin.vy > 0 then
+    al_draw_filled_rectangle(Coin.x, Coin.y-Coin.vy*10, Coin.x+Coin.w, Coin.y, CoinShadeColor)
+  else if Coin.vy < 0 then
+    al_draw_filled_rectangle(Coin.x, Coin.y+Coin.h, Coin.x+Coin.w, Coin.y+Coin.h-Coin.vy*10, CoinShadeColor);
+end;
+
 procedure DrawPad(var Pad: TPad; var PadColor: ALLEGRO_COLOR; var PadShadeColor: ALLEGRO_COLOR);
 begin
   al_draw_filled_rectangle(Pad.x, Pad.y, Pad.x+Pad.w, Pad.y+Pad.h, PadColor);
@@ -266,8 +327,11 @@ end;
 procedure Draw;
 const
   SIZE = 60;
+var
+  I: Integer;
 begin
   al_clear_to_color(BackgroundColor);
+  DrawScore;
 
   if Shake > 0 then
   begin
@@ -285,15 +349,34 @@ begin
   al_draw_filled_rectangle(Settings.Width/2-SIZE, 0, Settings.Width/2+SIZE,
     Settings.Height, BackgroundShadeColor);
 
-  DrawPlayer(Player);
   DrawPad(Pad1, LeftPadColor, LeftPadShadeColor);
   DrawPad(Pad2, RightPadColor, RightPadShadeColor);
+
+  for I := 1 to MAX_COINS do
+  begin
+    if Coins[I].Active <> 0 then
+      DrawCoin(Coins[I], CoinColor, CoinShadeColor);
+  end;
+  DrawPlayer(Player);
 
   al_flip_display;
 end;
 
 procedure Update;
+var
+  I: Integer;
+  N: Integer;
 begin
+  N := random(MAX_COINS) + 1;
+  if Coins[N].Active = 0 then
+  begin
+    Coins[N].Active := random(40) + 300;
+    Coins[N].x := random(Settings.Width div 2) + Settings.Width div 4;
+    Coins[N].y := random(Settings.Height div 2) + Settings.Height div 4;
+    Coins[N].vx := 0;
+    Coins[N].vy := 0;
+  end;
+
   // move
   Player.x := Player.x + Player.vx;
   Player.y := Player.y + Player.vy;
@@ -303,6 +386,25 @@ begin
 
   Pad2.x := Pad2.x + Pad2.vx;
   Pad2.y := Pad2.y + Pad2.vy;
+
+  for I := 1 to MAX_COINS do
+  begin
+    if Coins[I].Active > 0 then
+    begin
+      Coins[I].x := Coins[I].x + Coins[I].vx;
+      Coins[I].y := Coins[I].y + Coins[I].vy;
+
+      Dec(Coins[I].Active);
+      if (Player.x+Player.w >= Coins[I].x) and (Player.x <= Coins[I].x+Coins[i].w) and
+       (Player.y+Player.h >= Coins[I].y) and (Player.y <= Coins[I].y+Coins[I].h) then
+      begin
+        Coins[I].Active := 0;
+        Inc(Score);
+        al_play_sample(PointSound, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, nil)
+      end;
+
+    end;
+  end;
 
   // bots
   if Pad1.y+Pad1.h >= Settings.Height then
@@ -351,6 +453,7 @@ begin
     Player.vx := -Player.vx;
     Player.x := Pad1.x+Pad1.w;
     Pad1.vy := Pad1.vy + 1;
+    Inc(Score);
     al_play_sample(LeftPadSound, 1.0, -0.75, 1.0, ALLEGRO_PLAYMODE_ONCE, nil)
   end
   else if (Player.x+Player.w >= Pad2.x) and (Player.x <= Pad2.x+Pad2.w) and
@@ -361,6 +464,7 @@ begin
     Player.vx := -Player.vx;
     Player.x := Pad2.x-Player.w;
     Pad2.vy := Pad2.vy - 1;
+    Inc(Score);
     al_play_sample(RightPadSound, 1.0, 0.75, 1.0, ALLEGRO_PLAYMODE_ONCE, nil)
   end;
 
@@ -380,6 +484,9 @@ begin
     while Player.vy = 0 do
       Player.vy := (Random(3)-1) * 10;
     StartUpDelay := 100;
+    if Score > BestScore then
+      BestScore := Score;
+    Score := 0;
   end;
 
   if Player.vx = 0 then
